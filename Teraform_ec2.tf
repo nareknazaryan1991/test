8427545812be9f271223781e8ea31e31b4390c57
Narek Nazaryan <narek.nazaryan.1991@mail.ru>
2024-02-22 23:06:28 +0400

    fix2

--- Teraform_ec2.tf
+++ Teraform_ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2-db" {
 
   ami                         = "ami-0781b2f5146911b9a"
   instance_type               = "t2.micro"
-  subnet_id                   = module.vpc.public_subnets[0]
+  subnet_id                   = aws_subnet.terraform_subnet.id
   vpc_security_group_ids      = [aws_security_group.terraform_vpc_security_group.id]
   associate_public_ip_address = true
   key_name                    = "narek-es2-2.2-key"
